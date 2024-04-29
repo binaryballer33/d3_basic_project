585addf8e84002d6b6627942df6d4e54dac35419
Shaquille Mandy <shaqmandy@gmail.com>
2024-04-29 20:43:30 +0800

    ts configuration changes for project

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { default as createBasicPieChart } from './basic_pie_chart.js'
-export { default as createBasicShapes } from './basic_shapes.js'
-export { default as createBasicBarGraph } from './basic_bar_graph.js'
+export { default as createBasicPieChart } from './basic_pie_chart'
+export { default as createBasicShapes } from './basic_shapes'
+export { default as createBasicBarGraph } from './basic_bar_graph'
